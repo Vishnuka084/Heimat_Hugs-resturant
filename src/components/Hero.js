fb0a30374e304008c9b754dd2a6cc99e5fcea781
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-21 01:22:57 +0530

    setup hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,31 @@
 import React from 'react';
+//import data
+import { heroData } from '../data';
+//import component
+import Header from "./Header";
+//import motion
+import { motion} from "framer-motion";
+//import variants
+import { fadeIn ,staggerContainer} from "../variants";
+
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const { pretitle ,title ,subtitle btnText} = heroData
+  return (
+      <section className='min-h-[980px] bg-hero bg-cover bg-right'>
+        {/* header */}
+        <header/>
+        <div className='container mx-auto'>
+          {/*text*/}
+          <div>
+            <div>{pretitle}</div>
+            <div>{title}</div>
+            <div>{subtitle}</div>
+            <div>{btnText}</div>
+          </div>
+        </div>
+      </section>
+  );
 };
 
 export default Hero;
