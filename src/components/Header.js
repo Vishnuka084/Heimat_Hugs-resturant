18af927f8b66346db489084152d93d80f7b2303a
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-28 15:14:59 +0530

    Updated Contnet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
+
+// import components
+import Nav from '../components/Nav';
+import Socials from "./Socials";
+//import logo
+import LogoWhite from '../assets/img/header/logo-white.png';
+//import motion
+import {motion} from "framer-motion";
+//import variants
+import { staggerContainer,fadeIn} from "../variants";
+
+// header variants
+const headerVariants = {
+  hidden : {
+    padding : '84px 0 84px 0',
+    background: 'none',
+  },
+  show: {
+    padding: '14px 0 14px 0',
+    background: 'rgba(0,0,0,0.92)',
+    transition: {
+      type:'spring',
+    },
+  },
+};
+
 
 const Header = () => {
-  return <div>Header</div>;
+  //header
+  const [isActive, setIsActive] = useState(false);
+  // event  listener
+  useEffect(()=> {
+    window.addEventListener('scroll',() => {
+      window.scrollY > 50 ? setIsActive(true) : setIsActive(false);
+
+    });
+  });
+  return (
+
+
+  );
 };
 
 export default Header;
