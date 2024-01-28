e98c2cb6e6dee9870a360f9d3ee8ef5aa5ad4b2a
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-28 11:45:21 +0530

    Updated Contnet

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,16 +15,20 @@ const Hero = () => {
       <section className='min-h-[980px] bg-hero bg-cover bg-right'>
         {/* header */}
         <header/>
-        <div className='container mx-auto'>
+        <motion.div
+            variants={staggerContainer(0.31)}
+            initial='hidden'
+            className='container mx-auto min-h-[980px] flex
+        justify-center items-center'>
           {/*text*/}
-          <div>
+          <div className='text-center flex flex-col items-center'>
             <div className='text-white text-[24px] lg:text-[28px]
             font-primary italic lg:font-medium mb-1'>{pretitle}</div>
             <div className='h1 mb-5'>{title}</div>
             <div className='text-white max-w-[54px] mb-8'>{subtitle}</div>
             <div className='btn'>{btnText}</div>
           </div>
-        </div>
+        </motion.div>
       </section>
   );
 };
