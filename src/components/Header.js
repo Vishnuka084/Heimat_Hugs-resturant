0d54723befa2e8ed6a810c34de065e90eaf19e3c
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-02-03 12:18:13 +0530

    updated header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
-import React,{}from 'react';
+import React,{ useState,  useEffect}from 'react';
+//import component
+import Nav from '../components/Nav';
+import Socials from '../components/Socials';
+
+//logo
+import LogoWhite  from '../assets/img/header/logo-white.png';
+
+// motion
+import {motion} from 'framer-motion';
+
+// variants
+import { staggerContainer, fadeIn} from '../variants';
+
+// header variants
+const headerVariants = {
+  hidden: {
+    padding: '84px 0 84px 0',
+    background: 'none',
+  },
+  show: {
+    padding: '14px 0 14px 0',
+    background: 'rgb(0,0,0,0.92)',
+    transition: {
+        type: 'spring',
+    },
+  },
+};
+
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='bg-pink-200'>Header</header>;
 };
 
 export default Header;
